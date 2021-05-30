4a3826cb6bb6b620e54f45f53f2bd9566429a78e
Urquiza <urquiza-nobrega@outlook.com>
2021-05-29 22:43:36 -0300

    style input error

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,18 +1,44 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+import Tooltip from '../Tooltip';
+
+interface ContainerProps {
+  isFocused: boolean;
+  isFilled: boolean;
+  hasError?: boolean
+}
+
+export const Container = styled.div<ContainerProps>`
   display: flex;
   align-items: center;
   background: #232129;
   border-radius: 10px;
-  border: 2px solid #232129;
   width: 100%;
-  color: #666360;
   padding: 0px 8px;
+
+  border: 2px solid #232129;
+  color: #666360;
 
   & + & {
     margin-top: 8px;
   }
+
+  ${(props) => props.hasError && css`
+    border: 2px solid #c53030;
+  `}
+
+  ${(props) =>
+    props.isFocused
+    && css`
+      border: 2px solid #ff9000;
+      color: #ff9000;
+    `}
+
+  ${(props) =>
+    props.isFilled
+    && css`
+      color: #ff9000;
+    `}
 
   input {
     flex: 1;
@@ -29,4 +55,23 @@ export const Container = styled.div`
   svg {
     margin: 0 8px;
   }
+`;
+
+export const Error = styled(Tooltip)`
+  margin-left: 16px;
+  height: 20px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
+  }
+
 `;
